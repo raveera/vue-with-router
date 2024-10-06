0feccc2277c2b1f4a0e3db5d028549b9e3a61f9e
Danish <raveera39@hotmail.com>
2024-10-06 23:06:09 +0700

    feat(api) add base api (put, delete)

--- src/api/base-api.js
+++ src/api/base-api.js
@@ -36,7 +36,48 @@ async function post (tempUrl, body) {
     }
 }
 
+async function put (tempUrl, body) {
+    const url = HOST_NAME + tempUrl
+
+    const option = {
+        method: 'PUT',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    try {
+        const response = await fetch(url, option)
+    
+        return response.json()
+    } catch (error) {
+        console.log('err =>', error)
+    }
+}
+
+async function remove (tempUrl) {
+    const url = HOST_NAME + tempUrl
+
+    const option = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    try {
+        const response = await fetch(url, option)
+    
+        return response.json()
+    } catch (error) {
+        console.log('err =>', error)
+    }
+}
+
 export default {
     get,
-    post
+    post,
+    put,
+    remove
 }
