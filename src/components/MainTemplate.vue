9736c7d9170e5768a7c222d54c6836ec12d6596d
Danish <raveera39@hotmail.com>
2024-10-06 19:38:48 +0700

    feat(compoent) add component

---
+++ src/components/MainTemplate.vue
@@ -0,0 +1,38 @@
+<template>
+  <VToolbar class="mb-2 px-2" style="border-radius: 8px;" color="grey-darken-2">
+    <VSpacer />
+    <VBtn
+      variant="outlined"
+      color="white"
+      @click="onLogoutButtonClick"
+    >
+      Logout
+    </VBtn>
+  </VToolbar>
+  <VCard class="main-box">
+    <VRow no-gutters justify="space-between" align-content="center" class="mb-2">
+      <slot name="title" />
+    </VRow>
+    <slot name="body" />
+  </VCard>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+import localStorageUtil from '@/util/localStorage-util'
+
+const router = useRouter()
+
+function onLogoutButtonClick () {
+  localStorageUtil.remove('userId')
+  router.push({name: 'login'})
+}
+</script>
+
+<style scoped>
+  .main-box {
+    width:  80vw;
+    height: 80vh;
+    padding: 16px;
+  }
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,60 @@
+<template>
+  <VDialog max-width="960" max-height="70vh">
+    <VCard>
+      <VCardTitle :class="[ACTION_THEME[action].bgColor]">
+        {{ title }}
+      </VCardTitle>
+      <VCardText>
+        <slot name="body" />
+      </VCardText>
+      <VCardActions class="text-uppercase border-t">
+        <VSpacer />
+        <VBtn
+          variant="outlined"
+          color="grey"
+          text="Cancel"
+          @click="$emit('onCancelClick')"
+        />
+        <VBtn
+          variant="outlined"
+          :color="ACTION_THEME[action].color"
+          :text="ACTION_THEME[action].confirmBtnText"
+          @click="$emit('onConfirmClick')"
+          />
+      </VCardActions>
+    </VCard>
+  </VDialog>
+</template>
+
+<script setup>
+defineProps({
+  action: {
+    type: String,
+    Required: true
+  },
+  title: {
+    type: String,
+    default: ''
+  }
+})
+
+
+const ACTION_THEME = {
+  'create': {
+    confirmBtnText: 'Create',
+    color: 'info',
+    bgColor: 'bg-info'
+  },
+  'edit': {
+    confirmBtnText: 'Save',
+    color: 'success',
+    bgColor: 'bg-success'
+  },
+  'delete': {
+    confirmBtnText: 'Delete',
+    color: 'error',
+    bgColor: 'bg-error'
+  }
+}
+
+</script>
